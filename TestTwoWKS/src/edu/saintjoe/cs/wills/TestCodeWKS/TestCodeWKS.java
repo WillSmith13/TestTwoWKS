4839fd1d1eb504af53fcd163b39547e95d0a916c
wills <wills@localhost>
2016-04-06 14:52:35 -0400

    Typing Code

--- TestTwoWKS/src/edu/saintjoe/cs/wills/TestCodeWKS/TestCodeWKS.java
+++ TestTwoWKS/src/edu/saintjoe/cs/wills/TestCodeWKS/TestCodeWKS.java
@@ -1,10 +1,23 @@
 package edu.saintjoe.cs.wills.TestCodeWKS;
 
 public class TestCodeWKS {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Two instance variables 
+		
+		//  Where the name of the tree will be kept 
+		String name; 
+		
+		// This keeps track of the number of trees that are cut down 
+		int numofTreesCut;
+/*-----------------------*/
+		// This sets the name of the tree 
+		public void setName(String OakTree) {
+			String name = OakTree;
 	}
+// Checks for trees that have been cut
+		public String CollectTreeCut(String stringdown) {
+			
+			// Changes string number to binary so we can do math with it
+			int OakTree = Integer.parseInt(string OakTree);
+		}
 
 }
